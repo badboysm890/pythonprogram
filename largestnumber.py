d7aab0ff1a39168c7ce5eeb791f60626885db3ef
badboysm890 <praveensm890@gmail.com>
2019-02-06 23:20:54 +0530

    Update largestnumber.py

--- largestnumber.py
+++ largestnumber.py
@@ -1,10 +1,10 @@
-a=int(input("Enter the number1:"))
-b=int(input("Enter the number2:"))
-c=int(input("Enter the number3:"))
-if(a>b):
-  if(a>c):
-    print(a)
-elif(b>c):
-  print(b)
+n1=int(input())
+n2=int(input())
+n3=int(input())
+if(n1>n2):
+  if(n1>n3):
+    print(n1)
+elif(n2>n3):
+  print(n2)
 else:
-  print(c)
+  print(n3)
